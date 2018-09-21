e711254d473d6ac3ee3bf6a8a205baca8de99b88
Gouthami Senthamaraikkannan <senthama@ualberta.ca>
2018-09-20 17:13:46 -0700

    added docstrings to all functions

--- data/process_data.py
+++ data/process_data.py
@@ -1,13 +1,32 @@
+'''
+This is a collection of functions that loads text messages and their categories, 
+returning a clean dataframe that merges the text (features) and categories (targets)
+'''
 import sys
 import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    '''
+    Load the messages and information related to their categories
+    Args:
+        messages_filepath: filepath for loading messages
+        categories_filepath: filepath for loading related categories
+    Returns:
+        merged dataframe combining messages and their categories
+    '''
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     return messages.merge(categories, on='id')
 
 def clean_data(df):
+    '''
+    Clean up the merged dataframe so that they can be saved and sent for training
+    Args:
+        df: merged dataframe combining messages and categories
+    Returns:
+        df: cleaned up df
+    '''
     # generate separate category columns
     separated_categories = pd.DataFrame(df['categories'].str.split(';', expand=True))
     separated_category_names = [name[0] for name in separated_categories.loc[0].str.split('-')]
@@ -33,11 +52,20 @@ def clean_data(df):
     return df
 
 def save_data(df, database_filename):
+    '''
+    Save dataframe into an SQL database
+    Args:
+        df: clean data frame to be saved
+        database_filename: database path to be saved in 
+    '''
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('database', engine, index=False)
 
 
 def main():
+    '''
+    Function executing steps required to clean and save dataframe
+    '''
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
@@ -65,3 +93,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
