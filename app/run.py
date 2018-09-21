a2ed757ce581e4029e26889780b7536ba64b6100
Gouthami Senthamaraikkannan <senthama@ualberta.ca>
2018-09-20 17:12:11 -0700

    updated app to show two visualizations in total

--- app/run.py
+++ app/run.py
@@ -1,6 +1,7 @@
 import json
 import plotly
 import pandas as pd
+import numpy as np
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -16,6 +17,13 @@ import pickle
 app = Flask(__name__)
 
 def tokenize(text):
+    '''
+    Tokenize the text message and remove stop words, captialization etc.
+    Args:
+        text: input text message
+    Returns:
+        clean_tokens: tokenized list of strings
+    '''
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -31,24 +39,21 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('database', engine)
 
 # load model
-models = []
 with open("../models/classifier.pkl", 'rb') as f:
-    while True:
-        try:
-            models.append(pickle.load(f))
-        except EOFError:
-            break
+    model = pickle.load(f)
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-
+    
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-
+    
+    df_summary = df.describe()
+    aid_present = df_summary.loc['count'][0]*df_summary.loc['mean'][2:]
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -69,13 +74,29 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        
+        {
+            'data': [
+                Bar(
+                    x=aid_present.index.values,
+                    y=aid_present.values
+                )
+            ],
+            'layout': {
+                'title': 'Count of messages in training dataset from each aid category',
+                'yaxis': {
+                    'title': "Count"
+                }
+            }
         }
-    ]
 
+    ]
+    
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-
+    
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -84,15 +105,14 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '')
+    query = request.args.get('query', '') 
 
     # use model to predict classification for query
-    scores = []
-    for model in models:
-        scores.append(model.predict([query])[0])
-    classification_results = dict(zip(df.columns[4:], scores))
+    score = model.predict([query])
+    categories = [f.encode().decode("utf-8") for f in df.columns.values[4:].tolist()]
+    classification_results = dict(zip(categories, list(score.squeeze())))
 
-    # This will render the go.html Please see that file.
+    # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
         query=query,
